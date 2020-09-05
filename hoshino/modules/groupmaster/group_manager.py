19f8129f82fb4b23755a6e16544031f2a610cf9f
laipz8200 <laipz8200@outlook.com>
2020-09-06 04:00:58 +0800

    支持5分钟禁言和解除禁言

--- hoshino/modules/groupmaster/group_manager.py
+++ hoshino/modules/groupmaster/group_manager.py
@@ -76,4 +76,50 @@ async def awarded_title(bot, ev):
         return
     else:
         special_title = ''.join(special_title)
-        await util.set_group_special_title(ev, special_title=special_title, user_id=user_id)
+        await util.set_group_special_title(
+            ev, special_title=special_title, user_id=user_id)
+
+
+@sv.on_suffix(['禁言', '塞口球'])
+async def mute(bot, ev):
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, '我才不会听你的命令呢! 哼~', at_sender=True)
+        return
+    user_id = None
+    for msg_seg in ev.message:
+        if msg_seg.type == 'at':
+            if msg_seg.data['qq'] == 'all':
+                await bot.send(ev, '全员禁言吗? 我还在学呢~')
+                return
+            else:
+                user_id = int(msg_seg.data['qq'])
+    if user_id:
+        # await asyncio.sleep(3)
+        if user_id in config.SUPERUSERS:
+            await bot.send(ev, '我才不会对主人下手呢!')
+            return
+        await util.members_banned(ev, user_id, 60*5)
+        await bot.send(ev, f'{MessageSegment.at(user_id)} 请你安静一点啦~')
+    else:
+        await bot.send(ev, '你要禁言谁呀?', at_sender=True)
+
+
+@sv.on_prefix('解除禁言')
+async def remove_banned(bot, ev):
+    if not priv.check_priv(ev, priv.SUPERUSER):
+        await bot.send(ev, '我只听主人的话哦~', at_sender=True)
+        return
+    user_id = None
+    for msg_seg in ev.message:
+        if msg_seg.type == 'at':
+            if msg_seg.data['qq'] == 'all':
+                await bot.send(ev, '人家还不会啦, 你自己动手嘛~')
+                return
+            else:
+                user_id = int(msg_seg.data['qq'])
+    if user_id:
+        # await asyncio.sleep(3)
+        await util.members_banned(ev, user_id, 0)
+        await bot.send(ev, f'{MessageSegment.at(user_id)} 你可以说话咯~')
+    else:
+        await bot.send(ev, '你要解除谁的禁言呀?', at_sender=True)
