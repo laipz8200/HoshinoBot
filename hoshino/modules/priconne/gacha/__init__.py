250aa32597ab9f336555a4e9f3c21d0b0ec648cf
laipz8200 <laipz8200@outlook.com>
2020-09-24 10:27:48 +0800

    fix bug in switch gacha

--- hoshino/modules/priconne/gacha/__init__.py
+++ hoshino/modules/priconne/gacha/__init__.py
@@ -2,7 +2,7 @@ import os
 import random
 from collections import defaultdict
 
-from hoshino import Service, priv, util
+from hoshino import Service, priv, util, logger
 from hoshino.typing import *
 from hoshino.util import DailyNumberLimiter, concat_pic, pic2b64, silence
 
@@ -67,7 +67,7 @@ async def gacha_info(bot, ev: CQEvent):
 POOL_NAME_TIP = '请选择以下卡池\n> 切换卡池jp\n> 切换卡池tw\n> 切换卡池b\n> 切换卡池mix'
 
 
-@sv.on_fullmatch(('切换卡池', '选择卡池'))
+@sv.on_prefix(('切换卡池', '选择卡池'))
 async def set_pool(bot, ev: CQEvent):
     if not priv.check_priv(ev, priv.ADMIN):
         await bot.finish(ev, '只有群管理才能切换卡池', at_sender=True)
