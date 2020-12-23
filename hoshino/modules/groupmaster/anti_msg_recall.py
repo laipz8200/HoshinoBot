f3ed5918ef766ea540cafdd436a929fc2bf1d2b7
laipz8200 <laipz8200@outlook.com>
2020-12-23 16:21:35 +0800

    remove getbot

--- hoshino/modules/groupmaster/anti_msg_recall.py
+++ hoshino/modules/groupmaster/anti_msg_recall.py
@@ -1,4 +1,4 @@
-from hoshino import Service, get_bot
+from hoshino import Service
 
 sv = Service(
     'anti-msg-recall',
@@ -11,12 +11,13 @@ sv = Service(
 @sv.on_notice('group_recall')
 async def antiwithdraw(session):
     ev = session.event
+    bot = session.bot
     gid = ev['group_id']
     mid = ev['message_id']
     uid = ev['user_id']
     oid = ev['operator_id']
-    msg = await get_bot().get_msg(message_id=mid)
-    user = await get_bot().get_group_member_info(group_id=gid, user_id=uid)
+    msg = await bot.get_msg(message_id=mid)
+    user = await bot.get_group_member_info(group_id=gid, user_id=uid)
     if oid == uid and uid != ev['self_id']:
         await session.send(
             f'{user["card"]}({uid})撤回的消息是:\n{msg["message_raw"]}'
